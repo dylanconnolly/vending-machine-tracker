f878b0c9b05c36695bbf6af559cec5f37ac994e5
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-19 09:50:57 -0700

    Add another machine to average_snack_price test to make sure average is working as intended

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -12,13 +12,20 @@ RSpec.describe Machine, type: :model do
   end
 
   describe "#average_snack_price" do
-    owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    it "returns average price of its snacks" do
+      owner = Owner.create(name: "Sam's Snacks")
+      dons  = owner.machines.create(location: "Don's Mixed Drinks")
+      bobs = owner.machines.create(location: "Bobs Corner")
 
-    kitkat = dons.snacks.create(name: "KitKat", price: 2.50)
-    snickers = dons.snacks.create(name: "Snickers", price: 3)
-    reeses = dons.snacks.create(name: "Reese's", price: 1.25)
+      kitkat = dons.snacks.create(name: "KitKat", price: 2.50)
+      snickers = dons.snacks.create(name: "Snickers", price: 3)
+      reeses = dons.snacks.create(name: "Reese's", price: 1.25)
 
-    expect(dons.average_snack_price).to eq(2.25)
+      bobs.snacks << kitkat
+      bobs.snacks << snickers
+
+      expect(dons.average_snack_price).to eq(2.25)
+      expect(bobs.average_snack_price).to eq(2.75)
+    end
   end
 end
