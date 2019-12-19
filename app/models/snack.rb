07b1482e284d05b05231124eb9dafb72e72e0c37
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-19 09:54:26 -0700

    Create class method for snacks to get average_price. Update machine average_price method to call snack class method on its collection of snacks

--- app/models/snack.rb
+++ app/models/snack.rb
@@ -3,4 +3,8 @@ class Snack < ApplicationRecord
 
   has_many :machine_snacks
   has_many :machines, through: :machine_snacks
+
+  def self.average_price
+    self.average(:price)
+  end
 end
--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe Snack do
     it {should have_many :machine_snacks}
     it {should have_many(:machines).through(:machine_snacks)}
   end
+
+  describe "#average_price" do
+    it "returns average price of snacks in collection" do
+      kitkat = Snack.create(name: "KitKat", price: 2.50)
+      snickers = Snack.create(name: "Snickers", price: 3)
+      reeses = Snack.create(name: "Reese's", price: 1.25)
+
+      expect(Snack.average_price).to eq(2.25)
+
+
+      popcorn = Snack.create(name: "Popcorn", price: 5)
+
+      expect(Snack.average_price).to eq(11.75/4)
+    end
+  end
 end
