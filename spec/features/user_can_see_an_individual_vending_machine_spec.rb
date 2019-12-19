b3e35fecf7c94aa850b9c32ae705104a646e3865
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-19 09:44:27 -0700

    Add test for user story 2, create machine model test for average snack price method

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -27,4 +27,17 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
     expect(page).to have_content(reeses.name)
     expect(page).to have_content(reeses.price)
   end
+
+  it "the user sees the average price of the snacks for that machine" do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+
+    kitkat = dons.snacks.create(name: "KitKat", price: 2.50)
+    snickers = dons.snacks.create(name: "Snickers", price: 3)
+    reeses = dons.snacks.create(name: "Reese's", price: 1.25)
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("Average price: $2.25")
+  end
 end
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe Machine, type: :model do
     it {should have_many :machine_snacks}
     it {should have_many(:snacks).through(:machine_snacks)}
   end
+
+  describe "#average_snack_price" do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+
+    kitkat = dons.snacks.create(name: "KitKat", price: 2.50)
+    snickers = dons.snacks.create(name: "Snickers", price: 3)
+    reeses = dons.snacks.create(name: "Reese's", price: 1.25)
+
+    expect(dons.average_snack_price).to eq(2.25)
+  end
 end
