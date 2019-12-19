3bea1f245de529763e5cc1ca5049350da7d1c2ee
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-19 09:22:48 -0700

    Create spec for user story 1

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,22 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  it "the user sees a list of that vending machine's snacks" do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+
+    kitkat = dons.snacks.create(name: "KitKat", price: 2.50)
+    snickers = dons.snacks.create(name: "Snickers", price: 3)
+    reeses = dons.snacks.create(name: "Reese's", price: 1.25)
+
+    visit machine_path(dons)
+
+    expect(page).to have_content(kitkat.name)
+    expect(page).to have_content(kitkat.price)
+    expect(page).to have_content(snickers.name)
+    expect(page).to have_content(snickers.price)
+    expect(page).to have_content(reeses.name)
+    expect(page).to have_content(reeses.price)
+  end
 end
